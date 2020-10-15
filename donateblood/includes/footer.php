9226670111b2e9c0df97675abef1861bc8421157
JS SOLUTIONS INC <55834775+jssolutionsonline-com@users.noreply.github.com>
2020-10-15 10:01:39 +0100

    Add files via upload

---
+++ donateblood/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; JS Dev Team- Blood Donor Management System 2020 </p>
+        </div>
+        <!-- /.container -->
+    </footer>
